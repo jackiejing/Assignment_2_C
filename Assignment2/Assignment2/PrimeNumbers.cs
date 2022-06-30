4835a356be2b6f86d4614a592ddcf7c889229ec8
jackiejing <jackie.jingg1125@gmail.com>
2022-06-29 23:25:05 -0400

    Create PrimeNumbers.cs

---
+++ Assignment2/Assignment2/PrimeNumbers.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    public static class PrimeNumbers
+    {
+        static void Main()
+        {
+            int startNum = int.Parse(Console.ReadLine());
+            int endNum = int.Parse(Console.ReadLine());
+            PrintList(startNum, endNum);
+        }
+        static List<int> FindPrimesInRange(int startNum, int endNum)
+        {
+            List<int> primes = new List<int>();
+            for (int i = startNum; i <= endNum; i++)
+            {
+                if (IsPrime(i))
+                {
+                    primes.Add(i);
+                }
+            }
+            return primes;
+        }
+        static bool IsPrime(long n)
+        {
+            if ((n & 1) == 0)
+            {
+                if (n == 2) return true;
+                else return false;
+            }
+            for (long i = 3; (i * i) <= n; i += 2)
+            {
+                if (n % i == 0) return false;
+            }
+            return n != 1;
+        }
+        static void PrintList(int startNum, int endNum)
+        {
+            for (int i = 0; i < FindPrimesInRange(startNum, endNum).Count; i++)
+            {
+                Console.Write("{0}{1}", FindPrimesInRange(startNum, endNum)[i], i == FindPrimesInRange(startNum, endNum).Count - 1 ? "" : ", ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
