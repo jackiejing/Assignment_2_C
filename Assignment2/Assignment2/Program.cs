2c6f9ef60193fb7a6abe668e761995b78f8be4b3
jackiejing <jackie.jingg1125@gmail.com>
2022-06-29 23:25:08 -0400

    Create Program.cs

---
+++ Assignment2/Assignment2/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+// 1.
+/*
+int[] array1 = new int[] {1,2,3,4,5,6,7,8,9,10};
+int[] array2 = new int[10];
+Array.Copy(array1, array2, array2.Length);
+foreach (int i in array2)
+{
+    Console.WriteLine(i);
+}
+*/
+using Assignment2;
+//Console.WriteLine(ReverseLetter.Reverse("sample"));
+//Console.WriteLine(ReverseWord.ReverseWordValue("C# is not C++, and PHP is not Delphi!"));
