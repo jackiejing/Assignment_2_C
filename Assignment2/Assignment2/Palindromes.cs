a4c9c8ba722b4f79b4771de1e9db4bb2de0af678
jackiejing <jackie.jingg1125@gmail.com>
2022-06-29 23:24:59 -0400

    Create Palindromes.cs

---
+++ Assignment2/Assignment2/Palindromes.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    public class Palindromes
+    {
+        public static bool isPalindrome(string input)
+        {
+            int i = 0;
+            int j = input.Length - 1;
+
+            while (i < j)
+            {
+
+                if (input[i++] != input[j--])
+                    return false;
+            }
+
+            return true;
+        }
+
+        public static String CheckPalinWords(string input)
+        {
+
+            string final_input = "";
+            string word = "";
+
+            input = input + " ";
+            int n = input.Length;
+
+            for (int i = 0; i < n; i++)
+            {
+
+                if (input[i] != ' ')
+                    word = word + input[i];
+                else
+                {
+
+                    if (!(isPalindrome(word)))
+                        final_input += word + " ";
+
+                    word = "";
+                }
+            }
+            return final_input;
+        }
+    }
+}
