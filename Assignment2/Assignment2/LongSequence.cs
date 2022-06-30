7d4ca6ce2af4c7455874b0f36844216556e58066
jackiejing <jackie.jingg1125@gmail.com>
2022-06-29 23:21:38 -0400

    Create LongSequence.cs

---
+++ Assignment2/Assignment2/LongSequence.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    public class LongSequence
+    {
+        public static void FindLongSequence(int[] number)
+        {
+            int count = 1;
+            int longestNum = number[0];
+            int longestCount = 1;
+            for (int i = 1; i < number.Length; i++)
+            {
+                if (number[i] != number[i - 1])
+                {
+                    count = 0;
+                }
+                count++;
+                if(count > longestCount)
+                {
+                    longestCount = count;
+                    longestNum = number[i];
+                }
+            }
+            Console.WriteLine(string.Join(" ", Enumerable.Repeat(longestNum, longestCount)));
+            int[] result = new int[longestCount];
+            Array.Fill(result, longestNum);
+        }
+    }
+}
