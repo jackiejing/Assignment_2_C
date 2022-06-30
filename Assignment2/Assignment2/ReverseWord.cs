57c32de1bafb160506e575c041e251fc9b10f121
jackiejing <jackie.jingg1125@gmail.com>
2022-06-29 23:25:12 -0400

    Create ReverseWord.cs

---
+++ Assignment2/Assignment2/ReverseWord.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    public class ReverseWord
+    {
+        public static string ReverseWordValue(string input)
+        {
+            return String.Join(" ", input.Split('.', ' ', '!', '?', '/').Reverse()).ToString();
+        }
+    }
+}
