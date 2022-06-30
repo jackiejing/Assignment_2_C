23551aa5665261163d3213f4706ed357f552639b
jackiejing <jackie.jingg1125@gmail.com>
2022-06-29 23:25:10 -0400

    Create ReverseLetter.cs

---
+++ Assignment2/Assignment2/ReverseLetter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    public class ReverseLetter
+    {
+        public static string Reverse(string input)
+        {
+            char[] chars = input.ToCharArray();
+            string reverseString = String.Empty;
+            for(int i = chars.Length - 1; i > -1; i--)
+            {
+                reverseString += chars[i];
+            }
+            return reverseString;
+        }
+    }
+}
