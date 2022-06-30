83310b6ac8d93426d68ae81b83b5b5aea3081053
jackiejing <jackie.jingg1125@gmail.com>
2022-06-29 23:21:40 -0400

    Create MostFrequent.cs

---
+++ Assignment2/Assignment2/MostFrequent.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    public class MostFrequent
+    {
+        public static void MostFrequentValue(int[] numbers)
+        {
+            var counts = new Dictionary<int, int>();
+            foreach(int number in numbers)
+            {
+                int count;
+                counts.TryGetValue(number, out count);
+                count++;
+                counts[number] = count;
+            }
+            int mostCommonValue = 0;
+            int occurrences = 0;
+            foreach(var pair in counts)
+            {
+                if(pair.Value > occurrences)
+                {
+                    occurrences = pair.Value;
+                    mostCommonValue = pair.Key;
+                }
+            }
+            Console.WriteLine($"The number {mostCommonValue} is most frequent (occurs {occurrences} times)");
+        }
+    }
+}
